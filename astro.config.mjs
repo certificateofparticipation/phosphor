2ce101c01776d57ba51e19c9dce574afc64d3475
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2024-06-28 20:15:53 +0800

    modify: change adapter to vercel for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
